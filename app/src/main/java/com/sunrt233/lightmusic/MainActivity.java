e94108f18b8976f8659a5d1d15d10ea349185f18
2109219866@qq.com <2109219866@qq.com>
2019-01-23 17:06:27 +0800

    Initial commit

---
+++ app/src/main/java/com/sunrt233/lightmusic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sunrt233.lightmusic;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
